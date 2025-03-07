3659edf249cacfe24ef37dd1ace07cb3b90b8c3f
rohitmehta18 <rmehta1836@gmail.com>
2025-03-07 12:04:28 +0530

    done

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h1 className="text-2xl font-bold">Rohit Mehta</h1>
+            <p className="mt-2">Contact: rmehta1836@gmail.com</p>
+            <p>Phone: +91 9877989570</p>
+        </div>
+    );
+};
+
+export default Home;
